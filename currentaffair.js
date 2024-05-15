d3515e3382ddac9b6813a213ffff8ae6c070b75d
Lakshay Aggarwal <138527576+lakshay-1809@users.noreply.github.com>
2024-05-15 14:06:12 +0530

    Questions for current affair were added

---
+++ currentaffair.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which institution released the 14th Emissions Gap Report 2023?",
+    answer: "UNEP",
+    options: [
+      "UNESCO",
+      "UNEP",
+      "World Bank",
+      "WHO"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which state is the host of the Global Fisheries Conference India 2023?",
+    answer: "Gujarat",
+    options: [
+      "Maharashtra",
+      "West Bengal",
+      "Tamil Nadu",
+      "Gujarat"
+    ]
+  },
+    {
+    numb: 3,
+    question: "As per WHO 2023 Global TB report, which county accounted for the highest number of tuberculosis (TB) cases in the world in 2022?",
+    answer: "India",
+    options: [
+      "India",
+      "China",
+      "Ethiopia",
+      "Pakistan"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which country has pulled out of the Treaty of Conventional Armed Forces in Europe?",
+    answer: "Russia",
+    options: [
+      "Russia",
+      "Israel",
+      "France",
+      "Germany"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which Indian city has been added to the UNESCO Creative Cities Network, in Literature category?",
+    answer: "Kozhikode",
+    options: [
+      "Kozhikode",
+      "Mysore",
+      "Banglore",
+      "Madurai"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which city is the host of World Health Summit 2023?",
+    answer: "Berlin",
+    options: [
+      "New Delhi",
+      "Berlin",
+      "Rome",
+      "New York"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the rank of India in the Global Hunger Index (GHI) 2023?",
+    answer: "111",
+    options: [
+      "121",
+      "131",
+      "101",
+      "111"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the rank of India in the Global Innovation Index 2023?",
+    answer: "40",
+    options: [
+      "40",
+      "60",
+      "50",
+      "30"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Vajra Prahar 2023 is a joint exercise between which countries?",
+    answer: "India and the United States",
+    options: [
+      "India and Sri Lanka",
+      "India and UK",
+      "India and the United States",
+      "India and France"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which scarce element has been discovered in the Sutlej River?",
+    answer: "Tantalum",
+    options: [
+      "Titanium",
+      "Tantalum",
+      "Scandium",
+      "Lithium"
+    ]
+  },
+];
+
