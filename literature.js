495802aa63b8abaeb0caa7061ebad97d4ea95e20
MokshThukral <156937786+MokshThukral@users.noreply.github.com>
2024-05-16 22:12:46 +0530

    Added quiz data in literature.js

---
+++ literature.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the last book in Gulliver's Travels?",
+    answer: "Voyage to Houyhnhnms",
+    options: [
+      "Voyage to Brobdingnag",
+      "Voyage to Balnibarbi",
+      "Voyage to Lilliput",
+      "Voyage to Houyhnhnms"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which British novelist created the iconic character James Bond?",
+    answer: "Ian Fleming",
+    options: [
+      "Ian Fleming",
+      "Alan Moore",
+      "Agatha Christie",
+      "J. R. R. Tolkien"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which novel by Charles Dickens tells the story of the Jarndyce family?",
+    answer: "Bleak House",
+    options: [
+      "David Copperfield",
+      "Bleak House",
+      "Little Dorrit",
+      "Hard Times"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who completed Christopher Marlowe’s poem Hero and Leander?",
+    answer: "George Chapman",
+    options: [
+      "George Chapman",
+      "Ben Jonson",
+      "Michael Drayton",
+      "John Marston"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who wrote the epic poem The Divine Comedy?",
+    answer: "Dante",
+    options: [
+      "Giovanni Boccaccio",
+      "Ludovico Ariosto",
+      "Dante",
+      "Henry of Settimello"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who is regarded as the father of the English novel?",
+    answer: "Henry Fielding",
+    options: [
+      "Wilkie Collins",
+      "Henry Fielding",
+      "Thomas Hardy",
+      "William Makepeace Thackeray"
+    ]
+  },
+  {
+    numb: 7,
+    question: "To which country does the Njáls saga and the Gísla saga belong?",
+    answer: "Iceland",
+    options: [
+      "Iceland",
+      "Poland",
+      "Hungary",
+      "Austria"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who wrote the epic poems Paradise Lost and Paradise Regained?",
+    answer: "John Milton",
+    options: [
+      "T. S. Eliot",
+      "John Milton",
+      "William Wordsworth",
+      "John Donne"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which work by Ovid is a collection of mythological stories?",
+    answer: "Metamorphoses",
+    options: [
+      "Amores",
+      "Tristia",
+      "Ars Amatoria",
+      "Metamorphoses"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who is the author of The Spirit of Laws?",
+    answer: "Montesquieu",
+    options: [
+      "Voltaire",
+      "Montesquieu",
+      "Jean-Jacques Rousseau",
+      "Immanuel Kant"
+    ]
+  },
+];
