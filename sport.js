b6959351676175b9bb3422b9a92aedd6adb17211
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2024-05-16 19:30:22 +0530

    Create sport.js which contained the all the data for the sports quiz

---
+++ sport.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the maximum permissible length of a bat in cricket?",
+    answer: "38 inches",
+    options: [
+      "41 inches",
+      "38 inches",
+      "36 inches",
+      "32 inches"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many squares or squares are there in the game of chess?",
+    answer: "64",
+    options: [
+      "64",
+      "65",
+      "66",
+      "67"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who won the first Olympic medal individually for independent India?",
+    answer: "K D Jadhav",
+    options: [
+      "Milkha Singh",
+      "Dhyan Chand",
+      "K D Jadhav",
+      "Vishwanathan Anand"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The quickest possible checkmate is in?",
+    answer: "2 moves",
+    options: [
+      "3 moves",
+      "5 moves",
+      "4 moves",
+      "2 moves"
+    ]
+  },
+    {
+    numb: 5,
+    question: "who is the most goal scorer in football international after Ronaldo and Messi?",
+    answer: "Sunil Chetri",
+    options: [
+      "Kylian Mbappe",
+      "Neymar Jr",
+      "Sunil Chetri",
+      "Erling Haaland"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the length of the cricket pitch?",
+    answer: "20.12 meters",
+    options: [
+      "20.12 meters",
+      "20.19 meters",
+      "20.30 meters",
+      "20.50 meters"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who became the first cricketer to hit 6 sixes in one day international criket?",
+    answer: "Herschelle Gibs",
+    options: [
+      "Yuvraj Singh",
+      "Herschelle Gibs",
+      "Vivian Richards",
+      "Kieron Pollards"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many Kabaddi World Cup titles are held by Indian Men's Kabaddi team?",
+    answer: "3",
+    options: [
+      "3",
+      "5",
+      "4",
+      "6"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which chess player recently overcame Vishy Anand and became India No.1?",
+    answer: "Dommaraju Gukesh",
+    options: [
+      "Vidit Santosh Gujrathi",
+      "Arjun Erigaisi",
+      "Dommaraju Gukesh",
+      "Harikrishna Pentala"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who won 2022 FIDE Candidates Chess tournament?",
+    answer: "Ian Nepomniachtchi",
+    options: [
+      "Ian Nepomniachtchi",
+      "Alireza Firouzja",
+      "Jan-Krzysztof Duda",
+      "Ding Liren"
+    ]
+  },
+];
+
