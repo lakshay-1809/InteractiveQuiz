e3277ab2622efcf23756a72245b66e9e85983f86
Nimish <156438677+nimish-kochhar@users.noreply.github.com>
2024-05-16 18:39:04 +0530

    added questions for history quiz

---
+++ history.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In which of the following inscriptions Ashoka made his famous declaration, All men are my children?",
+    answer: "Separate Kalinga Rock Edict I",
+    options: [
+      "Minor Rock Edict",
+      "Pillar Edict VII",
+      "Lumbini Pillar Edict",
+      "Separate Kalinga Rock Edict I"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following Rigvedic deities represented Storm?",
+    answer: "Maruts",
+    options: [
+      "Indra",
+      "Maruts",
+      "Varun",
+      "Apas"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The achievements of which ruler was recorded in Nasik Inscription?",
+    answer: "Gautamiputra Satkarni",
+    options: [
+      "Ashoka",
+      "Bindusara",
+      "Devabhummi",
+      "Gautamiputra Satkarni"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following passes was used by Sakas to come to India?",
+    answer: "Bolan Pass",
+    options: [
+      "Bolan Pass",
+      "Nathu La Pass",
+      "Shipki La Pass",
+      "Bara-lacha la Pass"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Allahabad Pillar Inscription gives a detailed account of the reign of which of the following?",
+    answer: "Samudra gupta",
+    options: [
+      "Kanishka",
+      "Samudra gupta",
+      "Kumaragupta",
+      "Ashoka"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What was the capital of the southern part of Avanti Mahajanapada?",
+    answer: "Mahishmati",
+    options: [
+      "Mahishmati",
+      "Mathura",
+      "Ujjain",
+      "Taxila"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In which century the first punched marked coins were used?",
+    answer: "6th century BCE",
+    options: [
+      "5th century BCE",
+      "7th century BCE",
+      "8th century BCE",
+      "6th century BCE"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following area was not surrendered to Chandragupta Maurya by Seleucus Nicator?",
+    answer: "Northern Afghanistan",
+    options: [
+      "Gandhara",
+      "Kabul",
+      "Herat",
+      "Northern Afghanistan"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which Rock Edict announces that the gift of Dhamma is the best gift of all gifts?",
+    answer: "Eleventh Edict",
+    options: [
+      "Eighth Edict",
+      "Ninth Edict",
+      "Eleventh Edict",
+      "Twelfth Edict"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the Upveda of  Samaveda called?",
+    answer: "Gandhraveda ",
+    options: [
+      "Ayurveda",
+      "Shilpaveda",
+      "Gandhraveda ",
+      "Dhanuraveda"
+    ]
+  },
+];
+
