6aff06747267b0440c272310eadd35f1bbeb5672
Nimish <156438677+nimish-kochhar@users.noreply.github.com>
2024-05-16 18:17:15 +0530

    Questions for geography quiz

---
+++ geography.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not one of the Channel Islands?",
+    answer: "Anglesey",
+    options: [
+      "Jersey",
+      "Guernsey",
+      "Anglesey",
+      "Alderney"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Windhoek is the capital of:?",
+    answer: "Namibia",
+    options: [
+      "Australia",
+      "Mali",
+      "Namibia",
+      "Republic of the Congo"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the capital of Iceland?",
+    answer: "Reykjavík",
+    options: [
+      "Vilnius",
+      "Reykjavík",
+      "Nuuk",
+      "Zagreb"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The capital of Nevada is:",
+    answer: "Carson City",
+    options: [
+      "Boston",
+      "Augusta",
+      "Carson City",
+      "Baton Rouge"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The capital of Colorado is:",
+    answer: "Denver",
+    options: [
+      "Trenton",
+      "Columbus",
+      "Carson City",
+      "Denver"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The capital of Alaska is:",
+    answer: "Juneau",
+    options: [
+      "Juneau",
+      "Indianapolis",
+      "Salem",
+      "Saint Paul"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which country’s capital city derives its name from dakhar, a Wolof name for the tamarind tree?",
+    answer: "Senegal",
+    options: [
+      "Madagascar",
+      "Bangladesh",
+      "Denmark",
+      "Senegal"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is India’s smallest state by area?",
+    answer: "Goa",
+    options: [
+      "Puducherry",
+      "Kerala",
+      "Goa",
+      "Uttar Pradesh"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the world’s highest capital?",
+    answer: "La Paz, Bolivia",
+    options: [
+      "Asunción, Paraguay",
+      "Montevideo, Uruguay",
+      "La Paz, Bolivia",
+      "Bogotá, Columbia"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Besides the Italian Peninsula, what are the other two great peninsulas of southern Europe?",
+    answer: "Iberian and Balkan",
+    options: [
+      "Iberian and Balkan",
+      "Baja and Malay",
+      "Iberian and Baja",
+      "Malay and Iberian"
+    ]
+  },
+];
+
