df420ea741526b0692c7fadbb5fe458fd576058d
Nimish <156438677+nimish-kochhar@users.noreply.github.com>
2024-05-16 19:35:21 +0530

    questions for politics quiz added

---
+++ politics.js
@@ -0,0 +1,56 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The name of the Laccadive, Minicoy and Amindivi islands was changed to Lakshadweep by an Act of Parliament in?",
+    answer: "1973",
+    options: [
+      "1973",
+      "1972",
+      "1971",
+      "1970"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The members of the Rajya Sabha are elected by",
+    answer: "elected members of the legislative assembly",
+    options: [
+      "the people",
+      "Lok Sabha",
+      "elected members of the legislative assembly",
+      "elected members of the legislative council"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The members of the panchayat are",
+    answer: "the electorates of the respective territorial constituencies",
+    options: [
+      "nominated by the district officer",
+      "the electorates of the respective territorial constituencies",
+      "nominated by local self-government minister of the state",
+      "nominated by the block development organization"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The power to decide an election petition is vested in the",
+    answer: "High courts",
+    options: [
+      "Parliament",
+      "Supreme Court",
+      "High courts",
+      "Election Commission"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The present Lok Sabha is the",
+    answer: "17th Lok Sabha",
+    options: [
+      "14th Lok Sabha",
+      "15th Lok Sabha",
+      "16th Lok Sabha",
+      "17th Lok Sabha"
+    ]
+  },
