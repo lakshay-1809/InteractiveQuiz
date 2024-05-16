0c1e2a24391958b1393da1ed85e3d6c6225fb599
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2024-05-16 22:03:45 +0530

    added quiz data in project.js

---
+++ project.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does BIOS stand for?",
+    answer: "Basic Inpute Output System",
+    options: [
+      "Built-In Operating System",
+      "Basic Inpute Output System",
+      "Binary Input/Output System",
+      "Basic Integrated Operating System"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does DPI stand for?",
+    answer: "Dots Per Inch",
+    options: [
+      "Digital Printing Integration",
+      "Basic Inpute Output System",
+      "Dots Per Inch",
+      "Display Pixel Index"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does JPEG stand for?",
+    answer: "Joint Photographic Experts Group",
+    options: [
+      "Joint Protocol Experts Graphics",
+      "Joint Project Experts Group",
+      "Joint Programming Experts Graphics",
+      "Joint Photographic Experts Group"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does USB stand for?",
+    answer: "Universal Serial Bus",
+    options: [
+      "Universal Serial Bus",
+      "Ultra Security Board",
+      "Universal Security Block",
+      "United Serial Bus"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does PNG stand for?",
+    answer: "Portable Network Graphics",
+    options: [
+      "Printable New Graphic",
+      "Port Natural Graphics",
+      "Printable Network Graphics",
+      "Portable Network Graphics"
+    ]
+  },
+];
+
